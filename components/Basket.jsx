626b2a26d3699a1667bd4b5dc79044a30258a98d
Mohamed abdallahi <97371201+BROSTORNADO@users.noreply.github.com>
2023-03-31 22:12:41 +0000

    Update Basket.jsx

--- components/Basket.jsx
+++ components/Basket.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useContext } from 'react'
 import CartContext from '../cartContext'
+import { Quant } from '../components'
+
 const Basket = () => {
   const { removeFromCart, items } = useContext(CartContext)
-  //   console.log(items)
+
   return (
     <div className="bg-gray-200 rounded-lg shadow-lg p-2 mt-2 max-h-72 overflow-y-auto">
       {items.map((item, idx) => (
@@ -13,12 +15,7 @@ const Basket = () => {
 
             <p className="text-green-500 font-bold">{item.prix}$</p>
           </div>
-          <button
-            className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition duration-300 ease-in-out"
-            onClick={() => removeFromCart(item.nom)}
-          >
-            Remove
-          </button>
+          <Quant />
         </div>
       ))}
     </div>
